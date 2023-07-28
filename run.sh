6396bf4ba9bd49c7fb939ec391c6ab7aa5267f73
fcj1649 <eswaramaharaja@gmail.com>
2023-07-28 14:51:47 -0400

    Daily update 2023-07-28-1451

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #sudo service tor restart
 #sleep 5
 #curl --socks5 localhost:9050 https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y-%m-%d-%H:%M').pdf
-curl --create-dirs https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y')/$(date '+%m')/$(date '+%Y-%m-%d-%H:%M').pdf
+curl --create-dirs https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y')/$(date '+%m')/$(date '+%Y-%m-%d-%H%M').pdf
 git add .
-git config user.email "noreply@shivamkhandelwal.in"
-git config user.name "Shivam Khandelwal (auto commit)"
-git commit -m "Daily update $(date '+%Y-%m-%d-%H:%M')"
+git config user.email "eswaramaharaja@gmail.com"
+git config user.name "fcj1649"
+git commit -m "Daily update $(date '+%Y-%m-%d-%H%M')"
 git push origin master

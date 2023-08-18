56b2cbaf965423069b5f28ccd4c2a1629f784c00
fcj1649 <eswaramaharaja@gmail.com>
2023-08-18 10:26:00 -0400

    Moved code from run to run_local to match master

---
+++ run_local.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\NoBackup\Git\sbi-tt-rates-historical
+bash run_local.sh
---
+++ run_local.sh
@@ -0,0 +1,9 @@
+#sudo service tor restart
+#sleep 5
+#curl --socks5 localhost:9050 https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y-%m-%d-%H:%M').pdf
+curl -s --create-dirs https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output $(date '+%Y')/$(date '+%m')/$(date '+%Y-%m-%d-%H%M').pdf
+git add .
+git config user.email "eswaramaharaja@gmail.com"
+git config user.name "fcj1649"
+git commit -m "Daily update $(date '+%Y-%m-%d-%H%M')"
+git push origin master
